63bebffe7b903a1e08f3fa80824ca291ddd6de1c
Sam Miller <noogii7@gmail.com>
2019-09-02 20:24:50 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+/** Graph Reduce
+ * Written by Melephas <Sam Miller>
+ * Contact: noogii7@gmail.com
+ * 
+ * License: BSD-3-Clause
+ */
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
