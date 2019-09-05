e2fc627275288ff08d15bf95e1698088c6fca523
Sam Miller <sam.r.miller1@outlook.com>
2019-09-05 15:59:37 +0100

    Added matrix trait

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,6 +1,33 @@
+use std::{
+  default::Default,
+  error::Error,
+  fmt::{
+    Display,
+    Formatter,
+    self
+  },
+  marker::Sized,
+  ops::{
+    Add,
+    AddAssign,
+    Sub,
+    SubAssign,
+    Mul,
+    MulAssign,
+    Neg
+  }
+};
+
+
 pub mod square_matrix;
 
 
-trait Matrix {
-  
+trait Matrix<T>: Sized + Add + AddAssign + Sub + SubAssign + Mul + MulAssign + Neg {
+  fn determinant(&self) -> T where T: num::Num + Default + ToString + Copy;
+  fn inverse(&self) -> Result<Self, Box<dyn Error>>;
+  fn invert(&mut self) -> Result<(), Box<dyn Error>>;
+  fn transposed(&self) -> Self;
+  fn transpose(&mut self) -> Self;
+  fn conjugate(&self) -> Self;
+  fn conjugate_self(&mut self) -> Self;
 }
